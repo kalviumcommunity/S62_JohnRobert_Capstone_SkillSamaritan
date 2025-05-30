bb04caf6d8dced2d89b885f23eedeff1a02c22c0
R John Robert <rjohn.robert@kalvium.community>
2025-05-31 02:43:09 +0530

    Setup Up React Project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This must include all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
